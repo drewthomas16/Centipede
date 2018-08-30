84544bcb182162ab28c6081ed63e8eb270005d4a
IsaacMilarky <ikerobot@gmail.com>
2018-08-30 14:01:23 -0500

    spawn

--- Centipede/Centipede/CentipedeGame.cpp
+++ Centipede/Centipede/CentipedeGame.cpp
@@ -133,11 +133,11 @@ bool CentipedeGame::update()
 	}
 	#pragma endregion
 
-	if (findFirstInstanceOf<Scorpion>() && rand() % 1000 < 5)
+	if (/*findFirstInstanceOf<Scorpion>() && */ rand() % 10000 < 5)
 		spawnObject<Scorpion>(rand() % 30 < 15 ? 0 : 29, rand() % 17);
 
 	//check if live spider
-	if (findFirstInstanceOf<Spider>() && (rand() % 1000) < 5)//no spider check if able to respawn
+	if (!findFirstInstanceOf<Spider>() &&  (rand() % 2000) < 5)//no spider check if able to respawn
 	{
 		std::shared_ptr<Spider> spider = spawnObject<Spider>(rand() 
 			% 30 < 15 ? 0 : 29, rand() % 5 + 18);
